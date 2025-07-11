fb2da0943b6a4dca80c9453ea90b299087416f86
Mati7123 <45175682+Mati7123@users.noreply.github.com>
2025-07-11 14:30:02 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -ex
 echo "ENTRYPOINT SH START"
 
 echo "📥 Pobieram plik z Azure Blob Storage..."
-azcopy copy "$AZCOPY_URL" "/app/playwright_test/Test1.cs"
+azcopy copy "$AZCOPY_URL" "/app/Test1.cs"
 
 echo "🧪 Uruchamiam testy..."
 dotnet test /app/playwright-test.csproj --logger:trx
